e4a433c011835949e6d6f5f1744fc218835a43d8
Bassot <g.bassot@jacquetmetals.com>
2023-07-13 10:46:17 +0200

    Express static

--- index.js
+++ index.js
@@ -2,56 +2,10 @@ const app = require('express')();
 const config = require("platformsh-config").config();
 const mysql = require("mysql2/promise");
 
-function openConnection() {
-  const credentials = config.credentials("database");
-  return mysql.createConnection({
-    host: credentials.host,
-    port: credentials.port,
-    user: credentials.username,
-    password: credentials.password,
-    database: credentials.path
-  });
-}
-
-function createTable(connection) {
-  return connection.execute(
-    `CREATE TABLE IF NOT EXISTS platforminfo (
-      uid INT(10) NOT NULL AUTO_INCREMENT,
-      username VARCHAR(64) NULL DEFAULT NULL,
-      departname VARCHAR(128) NULL DEFAULT NULL,
-      created DATE NULL DEFAULT NULL,
-      PRIMARY KEY (uid)
-    ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`
-  );
-}
-
-function insertData(connection) {
-  return connection.execute(
-    "INSERT INTO platforminfo (username, departname, created) VALUES ('platform', 'Deploy Friday', '2019-06-17')"
-  );
-}
-
-function readData(connection) {
-  return connection.query("SELECT * FROM platforminfo");
-}
-
-function dropTable(connection) {
-  return connection.execute("DROP TABLE platforminfo");
-}
-
 const root = path.join(__dirname, 'dist', 'quote');
 
 
-app.get('*' ,function(req, res) {
-  fs.stat(root + req.path, function(err){
-    if(err){
-      res.sendFile("index.html", { root });
-    }else{
-      res.sendFile(req.path, { root });
-    }
-  })
-});
-
+app.use('/', express.static('dist/quote'))
 // Get PORT and start the server
 app.listen(config.port, function() {
   console.log(`Listening on port ${config.port}`)
